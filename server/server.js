68566c197da870a609803eccfba3d0ea7dd78100
Ted Lano <tlano@bp-3.com>
2017-08-19 15:14:42 -0500

    updated port

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(publicPath));
-app.listen(3000, () => {
+
+app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
